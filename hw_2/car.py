625bf8cb41dbc131526973d222a7132ec050c239
Edisheri <kipiani182@gmail.com>
2025-03-22 19:41:29 -0100

    hw2

--- hw_2/car.py
+++ hw_2/car.py
@@ -1,6 +1,17 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from hw_2.base import Vehicle
+from hw_2.engine import Engine
 
-class Car:
-    pass
+class Car(Vehicle):
+
+    def __init__(self, weight, fuel, fuel_consumption, started = False, engine = None):
+        super().__init__(weight, fuel, fuel_consumption, started)
+        self.engine = engine
+
+    def set_engine(self, engine: Engine):
+        self.engine = engine
+
+
+
--- hw_2/plane.py
+++ hw_2/plane.py
@@ -1,7 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from hw_2 import exceptions
+from hw_2.base import Vehicle
 
 
-class Plane:
-    pass
+class Plane(Vehicle):
+
+    def __init__(self, weight, max_cargo, fuel, fuel_consumption, started = False, cargo = 0):
+        super().__init__(weight, fuel, fuel_consumption, started)
+        self.cargo = cargo
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, cargo):
+        expected_cargo = self.cargo + cargo
+        if expected_cargo > self.max_cargo:
+            raise exceptions.CargoOverload(expected_cargo, self.max_cargo)
+        self.cargo += cargo
+        return self
+
+    def remove_all_cargo(self):
+        previous_cargo = self.cargo
+        self.cargo = 0
+        return previous_cargo
