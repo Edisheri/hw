37ef278bbae13e2126dae11729fd1c362580599b
Edisheri <kipiani182@gmail.com>
2025-03-24 22:20:51 -0100

    fix: изменил распределение

--- hw3/generate_result.py
+++ hw3/generate_result.py
@@ -1,50 +1,42 @@
 import json
 import csv
+from itertools import cycle
 
-# Чтение пользователей
-with open('user.json', 'r') as f:
-    users = json.load(f)
+# Чтение пользователей из JSON
+with open('user.json', 'r') as user_file:
+    users = json.load(user_file)
 
-# Чтение книг
-books = []
-with open('books.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        books.append({
-            'title': row['Title'],
-            'author': row['Author'],
-            'pages': int(row['Pages']),
-            'genre': row['Genre']
-        })
-
-# Распределение книг
-total_users = len(users)
-total_books = len(books)
-base = total_books // total_users
-remainder = total_books % total_users
-
-result = []
-current_book = 0
-
-for i in range(total_users):
-    # Определяем количество книг для текущего пользователя
-    num_books = base + 1 if i < remainder else base
-
-    # Формируем список книг для пользователя
-    user_books = books[current_book:current_book + num_books]
-    current_book += num_books
-
-    # Формируем структуру пользователя
-    user_data = {
-        'name': users[i]['name'],
-        'gender': users[i]['gender'],
-        'address': users[i]['address'],
-        'age': users[i]['age'],
-        'books': user_books
+# Оставляем только необходимые поля у пользователей
+processed_users = []
+for user in users:
+    processed_user = {
+        "name": user.get("name", ""),
+        "gender": user.get("gender", ""),
+        "address": user.get("address", ""),
+        "age": user.get("age", 0),
+        "books": []
     }
+    processed_users.append(processed_user)
 
-    result.append(user_data)
+# Чтение книг из CSV
+with open('books.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file)
+    books = []
+    for row in reader:
+        book = {
+            "title": row["Title"],
+            "author": row["Author"],
+            "pages": int(row["Pages"]),
+            "genre": row["Genre"]
+        }
+        books.append(book)
 
-# Запись результата
-with open('result.json', 'w') as f:
-    json.dump(result, f, indent=2)
+# Распределение книг циклически между пользователями
+user_cycle = cycle(processed_users)
+for book in books:
+    user = next(user_cycle)
+    user['books'].append(book)
+
+# Сохранение результата в JSON с форматированием
+with open('result.json', 'w') as result_file:
+    json.dump(processed_users, result_file, indent=2)
